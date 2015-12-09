54756ee8bbea1d0116d1f565ce83db1f3d98ec7f
Thonatos.Yang <thonatos.yang@gmail.com>
2015-12-09 17:10:14 +0800

    change ver

--- test.js
+++
@@ -1,5 +0,0 @@
-var urlencode = require('urlencode');
- 
-console.log(urlencode('中文')); // default is utf8 
-console.log(urlencode('中文', 'gbk')); // '%CB%D5%C7%A7' 
-console.log(urlencode.decode("%25E4%25B8%25AD%25E6%2596%2587")); // 这是我从邮件里复制的字符串
